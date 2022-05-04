59c0bf5b740b92ff4a392c8f66c957e17eae4053
Apdo Elsaed <abdoelsaed114@gmail.com>
2022-05-04 15:28:19 +0200

    find unsorted subarray

---
+++ shortest_unsorted_continuous_subarray.js
@@ -0,0 +1,27 @@
+// solution 1
+// time complexity   => O(N)
+// space complexity  => O(N )
+
+let findUnsortedSubarray1 = (n) => {
+  let len = n.length - 1, left = -1, right = -1, max = n[0], min = n[len];
+  for (let i = 1; i <= len; i++) {
+    let a = n[i], b = n[len - i];
+    a < max ? (right = i) : (max = a);
+    b > min ? (left = i) : (min = b);
+  }
+  return Math.max(0, left + right - len + 1);
+};
+
+
+// solution 2
+// time complexity   => O(N log N)
+// space complexity  => O(N)
+
+let findUnsortedSubarray2 = (n) => {
+  let stack = [];
+  let sorted = [...n].sort((a, b) => a - b);
+  for (i = 0; i < n.length; i++) {
+    if (n[i] !== sorted[i]) stack.push(i);
+  }
+  return stack.length ? Math.max(...stack) - Math.min(...stack) + 1 : 0;
+};
