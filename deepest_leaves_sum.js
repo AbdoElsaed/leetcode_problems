364172de2fbd8e4421fe937efd3adbd8c2b2ba49
Apdo Elsaed <abdoelsaed114@gmail.com>
2022-05-17 12:01:25 +0200

    deepest_leaves_sum

---
+++ deepest_leaves_sum.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/deepest-leaves-sum/
+
+// with BFS
+let deepestLeavesSum = function(root) {
+    let q = [root], ans, qlen, curr
+    while (q.length) {
+        qlen = q.length, ans = 0
+        for (let i = 0; i < qlen; i++) {
+            curr = q.shift(), ans += curr.val
+            if (curr.left) q.push(curr.left)
+            if (curr.right) q.push(curr.right)
+        }
+    }
+    return ans
+};
+
+// with Recursive DFS: 
+let deepestLeavesSum2 = function(root) {
+    let sums = []
+    const dfs = (node, lvl) => {
+        if (lvl === sums.length) sums[lvl] = node.val
+        else sums[lvl] += node.val
+        if (node.left) dfs(node.left, lvl+1)
+        if (node.right) dfs(node.right, lvl+1)
+    }
+    dfs(root, 0)
+    return sums[sums.length-1]
+};
